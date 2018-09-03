d0caa076c3cf1d0ea945a441f0e532ae9362af25
Ege Emir Özkan <egeemirozkan@protonmail.com>
2018-09-04 00:25:43 +0300

    Da da da da ra ra ra

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var form = document.getElementById("theform");
 var selection = form.selection;
 var entry_ = form.entry_
 var button = form.button_;
-var content;
+var contents;
 var printOut = document.getElementById("ocelot");
 button.addEventListener("click", search_);
 
