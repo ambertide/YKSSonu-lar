a78cb01517554323941ddc1ea57d7772c2f65f0b
Ege Emir Özkan <egeemirozkan@protonmail.com>
2018-09-04 03:23:11 +0300

    fixed error regarding unexpect token <

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function search_(){
       var uInt8Array = new Uint8Array(this.response);
       var db = new SQL.Database(uInt8Array);
       var contents = db.exec("SELECT * FROM osymmaxmintablo4 WHERE `Program Adı` LIKE \"%" + entry_.value + "%\"");
-      printOut.innerHTML = <p>"Aramanızın İlk Beş Sonucu:"<\p> <p>contents[0].values[0];<\p> <p>contents[0].values[1];<\p> <p>contents[0].values[2];<\p> <p>contents[0].values[3];<\p> <p>contents[0].values[4];<\p>
+      printOut.innerHTML = "Aramanızın İlk Beş Sonucu: \n" + contents[0].values[0]; + "\n" + contents[0].values[1]; + "\n" + contents[0].values[2]; + "\n" + contents[0].values[3]; + "\n" + contents[0].values[4];
       something = contents;
     };
 xhr.send();
