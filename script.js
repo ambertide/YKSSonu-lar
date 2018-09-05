2e64cfa8c9ed5294164348a4c6734731183b7751
Ege Emir Özkan <egeemirozkan@protonmail.com>
2018-09-05 12:11:13 +0300

    stuff

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var selection = form.selection;
 var entry_ = form.entry_
 var button = form.button_;
 var printOut = document.getElementById("rustspottedcat");
-var somethin = {};
+var something = {};
 console.log("hello darkness my old frieeeendd")
 button.addEventListener("click", search_);
 
